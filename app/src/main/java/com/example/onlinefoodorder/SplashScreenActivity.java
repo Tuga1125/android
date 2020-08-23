f00b5333311f5527ae7daafea8f00a90aa91d4bb
Tuga1125 <52203539+Tuga1125@users.noreply.github.com>
2020-08-23 23:54:41 +0545

    Splash Screen setup

---
+++ app/src/main/java/com/example/onlinefoodorder/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.onlinefoodorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Handler handler= new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent= new Intent(SplashScreenActivity.this, LoginScreenActivity.class);
+                startActivity(intent);
+
+            }
+        }, 3000);
+
+    }
+}
