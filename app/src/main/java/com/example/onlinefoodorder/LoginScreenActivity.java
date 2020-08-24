8938d8e6375512cf1de7ebf3623ae1092b8b07ee
Tuga1125 <52203539+Tuga1125@users.noreply.github.com>
2020-08-24 13:38:50 +0545

    Register Activity and modifying login

--- app/src/main/java/com/example/onlinefoodorder/LoginScreenActivity.java
+++ app/src/main/java/com/example/onlinefoodorder/LoginScreenActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginScreenActivity extends AppCompatActivity {
     private EditText edtUsername, edtPassword;
+    private TextView lkRegr;
     private Button btnLogin;
 
     @Override
@@ -26,11 +29,10 @@ public class LoginScreenActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!LoginScreenActivity.this.edtUsername.getText().toString().equals("softwarica") || !LoginScreenActivity.this.edtPassword.getText().toString().equals("coventry")) {
+                if (!LoginScreenActivity.this.edtUsername.getText().toString().equals("Star") && !LoginScreenActivity.this.edtPassword.getText().toString().equals("1125")) {
 
-                    //Toast.makeText(MainActivity.this, "username or password is incorrect",Toast.LENGTH_SHORT).show();
 
-                    Toast.makeText(LoginScreenActivity.this, "username or password is incorrect", 0).show();
+                    Toast.makeText(LoginScreenActivity.this, "username or password is incorrect", Toast.LENGTH_SHORT).show();
                     LoginScreenActivity.this.edtUsername.setError("username or password is incorrect");
                     LoginScreenActivity.this.edtUsername.requestFocus();
                     return;
@@ -38,6 +40,16 @@ public class LoginScreenActivity extends AppCompatActivity {
                 LoginScreenActivity.this.startActivity(new Intent(LoginScreenActivity.this, Dashboard.class));
                 LoginScreenActivity.this.finish();
 
+            }
+        });
+
+        TextView register = (TextView)findViewById(R.id.lkRegr);
+        register.setMovementMethod(LinkMovementMethod.getInstance());
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreenActivity.this, RegisterActivity.class);
+                startActivity(intent);
             }
         });
     }
